56ae9c7059beb62c8b4d57431d14bee22a3607a3
Obinna Okere <okereobinna10@gmail.com>
2023-02-18 01:33:12 +0100

    Chatbox page adjusted

--- src/View/pages/chatbox/Chatbox.jsx
+++ src/View/pages/chatbox/Chatbox.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import NavBar from '../../components/navigation/NavBar'
 import './chatbox.css'
+import chat from '../../assets/images/chat.png'
 
 const Chatbox = () => {
   return (
     <div className='chatbox-container'>
         <NavBar />
-        Chatbox
+        <div className='chatbox-header'>Chatbox</div>
+        <div className="chatbox-wrapper">
+          <div className="chatbox-notification">
+
+            <div className="chatbox-notification-icon">
+              <img className='chatbox-icon-sizing' src={chat} alt="Chat png" />
+            </div>
+            <div className="chatbox-notification-description">
+              Keep calm, light a fire and watch this space. ✌️
+            </div>  
+            <div className="chatbox-notification-description-sub">
+              We'll be launching this section very soon.
+            </div> 
+          </div>
+        </div>
     </div>
   )
 }
