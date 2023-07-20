da59344e57f0a40abd65f5a1895e3495f6d91d88
Obinna Okere <okereobinna10@gmail.com>
2023-07-20 23:27:17 +0100

    Creating agent Pages 50%

--- src/View/pages/agent/Agent.jsx
+++ src/View/pages/agent/Agent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../../components/navigation/NavBar'
 import './agent.scss'
 import { Link } from 'react-router-dom';
@@ -9,6 +9,13 @@ import AgentTable from './AgentTable';
 import { AgentCard } from '../../components/card/Card';
 
 const Agent = () => {
+
+    const [list, setList] = useState(true)
+
+
+    const toggleList = () => {
+        setList(!list);
+    }
   return (
     <div className='agent-container'>
         <CreateAgentButton />
@@ -23,8 +30,38 @@ const Agent = () => {
                 <p>Agent</p>
             </div>
             <div className="crumb-right">
+
+                {
+                    list ? 
+                    <>
+                        <svg onClick={toggleList} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-left">
+                            <line className='crumblist-active' x1="23" x2="3" y1="6" y2="6"/>
+                            <line className='crumblist-active' x1="17" x2="3" y1="12" y2="12"/>
+                            <line className='crumblist-active' x1="10" x2="3" y1="18" y2="18"/>
+                        </svg>
+                        <svg onClick={toggleList} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-grid">
+                            <rect className='crumbgrid' width="7" height="7" x="3" y="3" rx="1"/>
+                            <rect className='crumbgrid' width="7" height="7" x="14" y="3" rx="1"/>
+                            <rect className='crumbgrid' width="7" height="7" x="14" y="14" rx="1"/>
+                            <rect className='crumbgrid' width="7" height="7" x="3" y="14" rx="1"/>
+                        </svg>
+                    </> : 
+                    <>
+                        <svg onClick={toggleList} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-left">
+                            <line className='crumblist' x1="23" x2="3" y1="6" y2="6"/>
+                            <line className='crumblist' x1="17" x2="3" y1="12" y2="12"/>
+                            <line className='crumblist' x1="10" x2="3" y1="18" y2="18"/>
+                        </svg>
+                        <svg onClick={toggleList} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-grid">
+                            <rect className='crumbgrid-active' width="7" height="7" x="3" y="3" rx="1"/>
+                            <rect className='crumbgrid-active' width="7" height="7" x="14" y="3" rx="1"/>
+                            <rect className='crumbgrid-active' width="7" height="7" x="14" y="14" rx="1"/>
+                            <rect className='crumbgrid-active' width="7" height="7" x="3" y="14" rx="1"/>
+                        </svg>
+                    </>
+                }
                
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-left">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-left">
                     <line className='crumblist' x1="23" x2="3" y1="6" y2="6"/>
                     <line className='crumblist' x1="17" x2="3" y1="12" y2="12"/>
                     <line className='crumblist' x1="10" x2="3" y1="18" y2="18"/>
@@ -34,7 +71,7 @@ const Agent = () => {
                     <rect className='crumbgrid' width="7" height="7" x="14" y="3" rx="1"/>
                     <rect className='crumbgrid' width="7" height="7" x="14" y="14" rx="1"/>
                     <rect className='crumbgrid' width="7" height="7" x="3" y="14" rx="1"/>
-                </svg>
+                </svg> */}
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-grid">
                     <rect className='crumbgrid-active' width="7" height="7" x="3" y="3" rx="1"/>
                     <rect className='crumbgrid-active' width="7" height="7" x="14" y="3" rx="1"/>
@@ -43,113 +80,118 @@ const Agent = () => {
                 </svg> */}
             </div>
         </div>
+        
+        {  list ? 
 
-        <div className="agent-wrapper">
-                <div className="agent-member">
-                    <div className="agent-member-avatar">
-                        <span>
-                            <FontAwesomeIcon icon={faUser}/>
-                            <div className="agent-member-rank"></div>
-                        </span>
-                    </div>
-                    <div className="agent-member-details">
-                        <div className='agent-member-name'>
-                            Mary Bassey
+            <div className="agent-wrapper">
+                <AgentTable />
+            </div>
+            
+            :
+            <div className="agent-wrapper">
+                    <div className="agent-member">
+                        <div className="agent-member-avatar">
+                            <span>
+                                <FontAwesomeIcon icon={faUser}/>
+                                <div className="agent-member-rank"></div>
+                            </span>
                         </div>
-                        <div className='agent-member-phone'>
-                            <span><FontAwesomeIcon icon={faPhone}/></span>
-                            <span><a href='tel:'>+234 812 782 2787</a></span>
-                        </div>
-                        <div className='agent-member-facility'>
-                            <span><FontAwesomeIcon icon={faLocationDot}/></span>
-                            <span>Victoria Island - Idejo</span>
-                        </div>
-                        <div className='agent-member-email'>
-                            <span><FontAwesomeIcon icon={faEnvelope}/></span>
-                            <span>itsupport@lagoonhospitals.com</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="agent-member">
-                    <div className="agent-member-avatar">
-                        <span>
-                            <FontAwesomeIcon icon={faUser}/>
-                            <div className="agent-member-rank"></div>
-                        </span>
-                    </div>
-                    <div className="agent-member-details">
-                        <div className='agent-member-name'>
-                            Mary Bassey
-                        </div>
-                        <div className='agent-member-phone'>
-                            <span><FontAwesomeIcon icon={faPhone}/></span>
-                            <span><a href='tel:'>+234 812 782 2787</a></span>
-                        </div>
-                        <div className='agent-member-facility'>
-                            <span><FontAwesomeIcon icon={faLocationDot}/></span>
-                            <span>Victoria Island - Idejo</span>
-                        </div>
-                        <div className='agent-member-email'>
-                            <span><FontAwesomeIcon icon={faEnvelope}/></span>
-                            <span>itsupport@lagoonhospitals.com</span>
+                        <div className="agent-member-details">
+                            <div className='agent-member-name'>
+                                Mary Bassey
+                            </div>
+                            <div className='agent-member-phone'>
+                                <span><FontAwesomeIcon icon={faPhone}/></span>
+                                <span><a href='tel:'>+234 812 782 2787</a></span>
+                            </div>
+                            <div className='agent-member-facility'>
+                                <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                                <span>Victoria Island - Idejo</span>
+                            </div>
+                            <div className='agent-member-email'>
+                                <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                                <span>itsupport@lagoonhospitals.com</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="agent-member">
-                    <div className="agent-member-avatar">
-                        <span>
-                            <FontAwesomeIcon icon={faUser}/>
-                            <div className="agent-member-rank"></div>
-                        </span>
-                    </div>
-                    <div className="agent-member-details">
-                        <div className='agent-member-name'>
-                            Mary Bassey
+                    <div className="agent-member">
+                        <div className="agent-member-avatar">
+                            <span>
+                                <FontAwesomeIcon icon={faUser}/>
+                                <div className="agent-member-rank"></div>
+                            </span>
                         </div>
-                        <div className='agent-member-phone'>
-                            <span><FontAwesomeIcon icon={faPhone}/></span>
-                            <span><a href='tel:'>+234 812 782 2787</a></span>
-                        </div>
-                        <div className='agent-member-facility'>
-                            <span><FontAwesomeIcon icon={faLocationDot}/></span>
-                            <span>Victoria Island - Idejo</span>
-                        </div>
-                        <div className='agent-member-email'>
-                            <span><FontAwesomeIcon icon={faEnvelope}/></span>
-                            <span>itsupport@lagoonhospitals.com</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="agent-member">
-                    <div className="agent-member-avatar">
-                        <span>
-                            <FontAwesomeIcon icon={faUser}/>
-                            <div className="agent-member-rank"></div>
-                        </span>
-                    </div>
-                    <div className="agent-member-details">
-                        <div className='agent-member-name'>
-                            Mary Bassey
-                        </div>
-                        <div className='agent-member-phone'>
-                            <span><FontAwesomeIcon icon={faPhone}/></span>
-                            <span><a href='tel:'>+234 812 782 2787</a></span>
-                        </div>
-                        <div className='agent-member-facility'>
-                            <span><FontAwesomeIcon icon={faLocationDot}/></span>
-                            <span>Victoria Island - Idejo</span>
-                        </div>
-                        <div className='agent-member-email'>
-                            <span><FontAwesomeIcon icon={faEnvelope}/></span>
-                            <span>itsupport@lagoonhospitals.com</span>
+                        <div className="agent-member-details">
+                            <div className='agent-member-name'>
+                                Mary Bassey
+                            </div>
+                            <div className='agent-member-phone'>
+                                <span><FontAwesomeIcon icon={faPhone}/></span>
+                                <span><a href='tel:'>+234 812 782 2787</a></span>
+                            </div>
+                            <div className='agent-member-facility'>
+                                <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                                <span>Victoria Island - Idejo</span>
+                            </div>
+                            <div className='agent-member-email'>
+                                <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                                <span>itsupport@lagoonhospitals.com</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-        </div>
-
-        <div className="agent-wrapper">
-            <AgentTable />
-        </div>
+                    <div className="agent-member">
+                        <div className="agent-member-avatar">
+                            <span>
+                                <FontAwesomeIcon icon={faUser}/>
+                                <div className="agent-member-rank"></div>
+                            </span>
+                        </div>
+                        <div className="agent-member-details">
+                            <div className='agent-member-name'>
+                                Mary Bassey
+                            </div>
+                            <div className='agent-member-phone'>
+                                <span><FontAwesomeIcon icon={faPhone}/></span>
+                                <span><a href='tel:'>+234 812 782 2787</a></span>
+                            </div>
+                            <div className='agent-member-facility'>
+                                <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                                <span>Victoria Island - Idejo</span>
+                            </div>
+                            <div className='agent-member-email'>
+                                <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                                <span>itsupport@lagoonhospitals.com</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="agent-member">
+                        <div className="agent-member-avatar">
+                            <span>
+                                <FontAwesomeIcon icon={faUser}/>
+                                <div className="agent-member-rank"></div>
+                            </span>
+                        </div>
+                        <div className="agent-member-details">
+                            <div className='agent-member-name'>
+                                Mary Bassey
+                            </div>
+                            <div className='agent-member-phone'>
+                                <span><FontAwesomeIcon icon={faPhone}/></span>
+                                <span><a href='tel:'>+234 812 782 2787</a></span>
+                            </div>
+                            <div className='agent-member-facility'>
+                                <span><FontAwesomeIcon icon={faLocationDot}/></span>
+                                <span>Victoria Island - Idejo</span>
+                            </div>
+                            <div className='agent-member-email'>
+                                <span><FontAwesomeIcon icon={faEnvelope}/></span>
+                                <span>itsupport@lagoonhospitals.com</span>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        
+        }
         
     </div>
   )
--- src/View/pages/agent/AgentTable.jsx
+++ src/View/pages/agent/AgentTable.jsx
@@ -109,7 +109,7 @@ function AgentTable() {
                     <th>Admin</th>
                     <th>Rank</th>
                     <th>Status</th>
-                    <th></th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -128,9 +128,143 @@ function AgentTable() {
                         </div>
                     </td>
                     <td className='table-data'>
+                        <div className='action-wrapper'>
+                            <div className='action-edit'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+                                    <path className='agentedit' d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
+                                </svg>
+                            </div>
+                            <div className='action-delete'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path className='agentdelete' d="M3 6h18"/><path className='agentdelete' d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                                    <path className='agentdelete' d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                                    <line className='agentdelete' x1="10" x2="10" y1="11" y2="17"/><line className='agentdelete' x1="14" x2="14" y1="11" y2="17"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><AgentCard /></div>
+                    </td>
+                    <td className='table-data'>Male</td>
+                    <td className='table-data'>Ikeja</td>
+                    <td className='table-data'>Admin</td>
+                    <td className='table-data'>Level 1</td>
+                    <td className='table-data'>
                         <div className='active-wrapper'>
                             <div className='active-sign'></div>
                             <div>Active</div>
+                        </div>
+                    </td>
+                    <td className='table-data'>
+                        <div className='action-wrapper'>
+                            <div className='action-edit'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+                                    <path className='agentedit' d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
+                                </svg>
+                            </div>
+                            <div className='action-delete'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path className='agentdelete' d="M3 6h18"/><path className='agentdelete' d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                                    <path className='agentdelete' d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                                    <line className='agentdelete' x1="10" x2="10" y1="11" y2="17"/><line className='agentdelete' x1="14" x2="14" y1="11" y2="17"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><AgentCard /></div>
+                    </td>
+                    <td className='table-data'>Male</td>
+                    <td className='table-data'>Ikeja</td>
+                    <td className='table-data'>Admin</td>
+                    <td className='table-data'>Level 1</td>
+                    <td className='table-data'>
+                        <div className='active-wrapper'>
+                            <div className='active-sign'></div>
+                            <div>Active</div>
+                        </div>
+                    </td>
+                    <td className='table-data'>
+                        <div className='action-wrapper'>
+                            <div className='action-edit'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+                                    <path className='agentedit' d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
+                                </svg>
+                            </div>
+                            <div className='action-delete'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path className='agentdelete' d="M3 6h18"/><path className='agentdelete' d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                                    <path className='agentdelete' d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                                    <line className='agentdelete' x1="10" x2="10" y1="11" y2="17"/><line className='agentdelete' x1="14" x2="14" y1="11" y2="17"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><AgentCard /></div>
+                    </td>
+                    <td className='table-data'>Male</td>
+                    <td className='table-data'>Ikeja</td>
+                    <td className='table-data'>Admin</td>
+                    <td className='table-data'>Level 1</td>
+                    <td className='table-data'>
+                        <div className='active-wrapper'>
+                            <div className='active-sign'></div>
+                            <div>Active</div>
+                        </div>
+                    </td>
+                    <td className='table-data'>
+                        <div className='action-wrapper'>
+                            <div className='action-edit'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+                                    <path className='agentedit' d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
+                                </svg>
+                            </div>
+                            <div className='action-delete'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path className='agentdelete' d="M3 6h18"/><path className='agentdelete' d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                                    <path className='agentdelete' d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                                    <line className='agentdelete' x1="10" x2="10" y1="11" y2="17"/><line className='agentdelete' x1="14" x2="14" y1="11" y2="17"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><AgentCard /></div>
+                    </td>
+                    <td className='table-data'>Male</td>
+                    <td className='table-data'>Ikeja</td>
+                    <td className='table-data'>Admin</td>
+                    <td className='table-data'>Level 1</td>
+                    <td className='table-data'>
+                        <div className='active-wrapper'>
+                            <div className='active-sign'></div>
+                            <div>Active</div>
+                        </div>
+                    </td>
+                    <td className='table-data'>
+                        <div className='action-wrapper'>
+                            <div className='action-edit'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pen">
+                                    <path className='agentedit' d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
+                                </svg>
+                            </div>
+                            <div className='action-delete'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path className='agentdelete' d="M3 6h18"/><path className='agentdelete' d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
+                                    <path className='agentdelete' d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
+                                    <line className='agentdelete' x1="10" x2="10" y1="11" y2="17"/><line className='agentdelete' x1="14" x2="14" y1="11" y2="17"/>
+                                </svg>
+                            </div>
                         </div>
                     </td>
                 </tr>
